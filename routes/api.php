5141920ebab27f8ff38596cf77b1bfd0033397d6
rainer <rainerr19@gmail.com>
2021-02-26 11:39:39 -0500

    api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::get('/','BookController@index')->name('index');
+Route::get('crear','BookController@create')->name('create');
+Route::post('/store','BookController@store')->name('store');
+Route::get('/detalles/{isbn}','BookController@show')->name('show');
+
+Route::delete('/eliminar/{isbn}','BookController@destroy')->name('destroy');
